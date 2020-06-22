852955ed712003e1b7ce676f28aebd2d88ce5393
Xavier Hancock <arbiter04032000@gmail.com>
2020-06-22 10:34:49 +1000

    Attempt at fixing ongoing prefab issues

--- Project/Assets/JMF/Systems/Event System/EventTriggerBoxR.cs
+++ Project/Assets/JMF/Systems/Event System/EventTriggerBoxR.cs
@@ -3,22 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 using JMF.Systems.EventSystem.UnityRenderer;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
 
 namespace JMF.Systems.EventSystem.UnityRenderer
 {
     public class EventTriggerBoxR : MonoBehaviour
     {
         public UnityEvent triggerEnterFunc, triggerExitFunc;
-        public Collider m_other;
 
         public void OnTriggerEnter(Collider other)
         {
+            print("Trigger " + this + " firing");
             triggerEnterFunc.Invoke();
         }
 
         public void OnTriggerExit(Collider other)
         {
             triggerExitFunc.Invoke();
+        }
+
+        public void Disabler(Collider other)
+        {
+            other.GetComponent<XRGrabInteractable>().e
         }
     }
 }
