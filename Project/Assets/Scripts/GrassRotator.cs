ef987d0f28a4cc7210b770faf5a1e5015255015c
Xavier Hancock <arbiter04032000@gmail.com>
2020-06-23 15:21:19 +1000

    Continuing scene touchups
    +Added grass
    +Added "grassrotator" script, to randomise grass rotations in editor

---
+++ Project/Assets/Scripts/GrassRotator.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class GrassRotator : MonoBehaviour
+{
+    void Start()
+    {
+        this.gameObject.transform.rotation = Quaternion.Euler(0, Random.Range(-360, 360), 0);
+    }
+}
